6b3087902fb07df40f264c8af40aa67bbf1f3b58
Muhammad Burhan Ahmed <92679045+Burh2k@users.noreply.github.com>
2023-07-22 05:40:16 +0500

    Update Final

--- scripting-file.js
+++ scripting-file.js
@@ -1,19 +1,23 @@
 function save() {
     var content = document.getElementById("input").value;
     var text = document.getElementById("list");
-
+    
     var newpara = document.createElement('p');
     newpara.textContent = content;
-    newpara.classList.add('paragragh-styling');
-    newpara.style.borderBottom("black");
+    newpara.style.borderBottom = "1px solid black";
 
     text.appendChild(newpara);
+    var count=0;
+    newpara.addEventListener('click', function () {
+        count=count+1;
+        this.classList.toggle('line-through');
+        if(count==2){
+            this.remove();
+        }
+    });
     document.getElementById("input").value = "";
 }
-var text = document.getElementById("list");
-text.addEventListener('click', function () {
-    text.style.textDecoration = 'line-through';
-});
+
 function clearlist() {
     document.getElementById("list").textContent = "";
 }
